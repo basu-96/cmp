503bf8079259590bac69d48c1cb9c0743ca57110
basu-96 <patrabibhabasu@gmail.com>
2017-01-28 11:31:17 +0530

    edit for dos

--- dos_dival.m
+++ dos_dival.m
@@ -1,22 +1,18 @@
  E = [8, 1; 1, 10];% E1 = 8, E2 = 10
  Y = [0.7, 0.5; 0.5, 0.6];
- n = 100;% # OF ATOMS
- data = [];
- for i = 2:1:n
-     a = [];
-     for j =1:1:2*i
-         a = [a; i];
-     end
-     m1 = kron(eye(i-1), Y);
-     m1 = [m1; zeros(2, 2*(i-1))];
-     m1 = horzcat(zeros(2*i, 2), m1);
-     m2 = m1';
-     m = kron(eye(i), E) + m1 + m2;
-     a = horzcat(a, eig(m));
-     data = [data; a];
- end
+ n = 500;% # OF ATOMS
 
-h = histogram(data(:,2), 100);
+ 
+   
+     m1 = kron(eye(n-1), Y);
+     m1 = [m1; zeros(2, 2*(n-1))];
+     m1 = horzcat(zeros(2*n, 2), m1);
+     m2 = m1';
+     m = kron(eye(n), E) + m1 + m2;
+
+
+
+h = histogram(eig(m), 100);
 xlabel('Energy');
 ylabel('Number of eigen states');
 title('without periodic boundary condition');
--- dos_mono.m
+++ dos_mono.m
@@ -2,28 +2,27 @@
 %conditions
 E = 10;
 t = 1;
-n = 200;
-data = [];
+n = 500;
 
-for i = 2:1:n
-    m = zeros(i);
-    a = i;
-    for j = 1:1:i
+
+
+    m = zeros(n);
+
+    for j = 1:1:n
         m(j, j) = E;
-        if j < i
+        if j < n
            
             m(j+1, j) = t;
             m(j, j+1) = t;
-            a = [a; i];
+
         end
         %m(1,i) = t;
         %m(i,1) = t;
     end
-    a = horzcat(a, eig(m));
-    data = [data; a];
+
     
-end
-h = histogram(data(:,2), 100);
+
+h = histogram(eig(m), 100);
 xlabel('Energy');
 ylabel('Number of eigen states');
 title('Without periodic boundary condition');
